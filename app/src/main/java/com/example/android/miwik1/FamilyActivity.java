86da657fdffdf662164548f507e38e56c5ed4311
Sbudza <sbudza@webmail.co.za>
2016-11-11 23:39:20 -0800

    Initial commit

---
+++ app/src/main/java/com/example/android/miwik1/FamilyActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.miwik1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FamilyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_family2);
+    }
+
+}
